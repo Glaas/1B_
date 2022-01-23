25e632bb4d67a07ec9b16a8277edbb8510d5bbba
Sebastien "Glaas" Decuyper <s.decuyper314@gmail.com>
2022-01-23 01:15:21 +0100

    Coins

---
+++ TT3_Performance_Requirement/Assets/Coin.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Coin : MonoBehaviour
+{
+    public int value;
+    private float delayBeforeDestroy;
+
+    private AudioSource audioSource;
+    private ParticleSystem ps;
+
+    private void Awake()
+    {
+        audioSource = GetComponent<AudioSource>();
+        ps = GetComponent<ParticleSystem>();
+    }
+
+    private void Start()
+    {
+        //Determine how long the coin will stay on screen before being destroyed
+        delayBeforeDestroy = GetComponent<ParticleSystem>().main.duration;
+        if (GetComponent<AudioSource>().clip.length > delayBeforeDestroy)
+        {
+            delayBeforeDestroy = GetComponent<AudioSource>().clip.length;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            //Add the value of the coin to the player's score
+            //Make coin non-interactable for the duration of effects to play
+            GetComponentInChildren<SpriteRenderer>().enabled = false;
+            GetComponent<Collider2D>().enabled = false;
+            //Play sound
+            GetComponent<AudioSource>().Play();
+            //Play particle system
+            GetComponent<ParticleSystem>().Play();
+
+
+            //Destroy the coin after a while
+            Destroy(gameObject, delayBeforeDestroy);
+        }
+    }
+}
